1875daa968048295b866ddee1c30fdea1990fef4
stanislav <sgryumov@bhft.com>
2024-03-20 22:32:29 +0300

    Add documentation warnonly

--- docs/make.jl
+++ docs/make.jl
@@ -1,24 +1,24 @@
 using NumExpr
 using Documenter
 
-DocMeta.setdocmeta!(NumExpr, :DocTestSetup, :(using NumExpr); recursive=true)
+DocMeta.setdocmeta!(NumExpr, :DocTestSetup, :(using NumExpr); recursive = true)
 
 makedocs(;
     modules = [NumExpr],
-    authors = "",
     sitename = "NumExpr.jl",
     format = Documenter.HTML(;
         repolink = "https://github.com/bhftbootcamp/NumExpr.jl.git",
         canonical = "https://bhftbootcamp.github.io/NumExpr.jl",
         edit_link = "master",
         assets = String["assets/favicon.ico"],
+        sidebar_sitename = false,
     ),
     pages = [
         "Home" => "index.md",
         "pages/expr_syntax.md",
         "pages/api_reference.md",
     ],
-    checkdocs = :missing_docs,
+    warnonly = [:doctest, :missing_docs],
 )
 
 deploydocs(;
