b6adc92711cde5bb48d0c128937fa7aa4c88396a
DeevsDeevs <deevsgt15@gmail.com>
2024-05-17 12:14:46 -0300

    NumExpr v1.0.0: Eval it all! :dart:

---
+++ docs/make.jl
@@ -0,0 +1,28 @@
+using NumExpr
+using Documenter
+
+DocMeta.setdocmeta!(NumExpr, :DocTestSetup, :(using NumExpr); recursive = true)
+
+makedocs(;
+    modules = [NumExpr],
+    sitename = "NumExpr.jl",
+    format = Documenter.HTML(;
+        repolink = "https://github.com/bhftbootcamp/NumExpr.jl",
+        canonical = "https://bhftbootcamp.github.io/NumExpr.jl",
+        edit_link = "master",
+        assets = String["assets/favicon.ico"],
+        sidebar_sitename = false,
+    ),
+    pages = [
+        "Home" => "index.md",
+        "pages/expr_syntax.md",
+        "pages/api_reference.md",
+    ],
+    warnonly = [:doctest, :missing_docs],
+)
+
+deploydocs(;
+    repo = "github.com/bhftbootcamp/NumExpr.jl",
+    devbranch = "master",
+    push_preview = true,
+)
